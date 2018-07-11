e21c341e6d93d761ff2501e6c144da831638b282
sergiofra98 <38866026+sergiofra98@users.noreply.github.com>
2018-07-11 01:45:41 -0600

    0.0.6

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -15,70 +15,8 @@ export class ReportesComponent implements OnInit {
   public reportesDatos: any;
 
   constructor() {
-    this.selectorTiempo = { hoy: true, ayer: false, antier: false, tresDias: false, semana: false, mes: false, anterior: false };
-    this.reportesDatos = {
-      hoy: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      },
-      {
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      ayer: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      },
-      {
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      },
-      {
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      },
-      {
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      antier: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      tresDias: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      semana: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      mes: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }],
-
-      anterior: [{
-        texto: 'Se detectó un retiro de la cuenta que termina ' +
-          'en xxxxxxxx2321 a nombre de Sergio Franco en una ubicación diferente a lo normal en un establecimiento diferente.',
-        transaccion: 'Retiro', riesgo: 'Alto', fecha: '13:32:32 UTC 17/6/2018'
-      }]
-    };
+    this.reportesDatos = [
+      {nombre: 'Luis Perez', transaccion: 'Retiro'}, ];
     this.form = new form('','','','','');
   }
 
